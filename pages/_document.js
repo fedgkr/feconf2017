100aeaf88507e96688f1f592dd151e935302c96f
uyeong <uyeong21c@gmail.com>
2017-06-19 22:27:39 +0900

    Add semicolon

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, {Head, Main, NextScript} from 'next/document'
-import flush from 'styled-jsx/server'
+import Document, {Head, Main, NextScript} from 'next/document';
+import flush from 'styled-jsx/server';
 
 class FEConfDocument extends Document {
     static getInitialProps ({renderPage}) {
@@ -14,14 +14,7 @@ class FEConfDocument extends Document {
                 <Head>
                     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
-                    <style>{`
-                        html,
-                        body {
-                            margin: 0;
-                            padding: 0;
-                            font-size: 16px;
-                        }
-                    `}</style>
+                    <style>{`html,body{margin:0;padding:0;font-size:16px;}`}</style>
                 </Head>
                 <body>
                     <Main/>
