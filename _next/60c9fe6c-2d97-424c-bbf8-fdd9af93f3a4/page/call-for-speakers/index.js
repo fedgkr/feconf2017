82d90f0b750993bf5aa69e448e3c4ac38003e250
uyeong <uyeong21c@gmail.com>
2017-06-22 16:40:35 +0900

    Update pages - 2017-06-22 16:40:35

--- _next/60c9fe6c-2d97-424c-bbf8-fdd9af93f3a4/page/call-for-speakers/index.js
+++ _next/60c9fe6c-2d97-424c-bbf8-fdd9af93f3a4/page/call-for-speakers/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/call-for-speakers', function() {
-            var comp = module.exports=webpackJsonp([3],[],[194]);
+            var comp = module.exports=webpackJsonp([3],[],[753]);
             return { page: comp.default }
           })
         
--- _next/60c9fe6c-2d97-424c-bbf8-fdd9af93f3a4/page/index.js
+++ _next/60c9fe6c-2d97-424c-bbf8-fdd9af93f3a4/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([4],[],[195]);
+            var comp = module.exports=webpackJsonp([4],[],[754]);
             return { page: comp.default }
           })
         
