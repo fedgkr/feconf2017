6228d385b2fa47594c2cb9557eb0f5c6ea8712e4
uyeong <uyeong21c@gmail.com>
2017-08-24 16:10:00 +0900

    Use media query on Countdown component

--- .storybook/stories/Countdown.js
+++ .storybook/stories/Countdown.js
@@ -6,8 +6,6 @@ import Countdown from '../../components/Countdown';
 storiesOf('Countdown', module)
     .addWithInfo('Default', '', () =>  (
         <div style={{padding: '100px 0', background: '#1c204f'}}>
-            <div style={{maxWidth: '690px', margin: '0 auto'}}>
-                <Countdown eventTime={moment('2017-10-21 10:00:00').unix()}/>
-            </div>
+            <Countdown style={{margin: '0 auto'}} eventTime={moment('2017-10-21 10:00:00').unix()}/>
         </div>
     ));
