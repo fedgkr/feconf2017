745726cc2a6703c2a14cab132a08699b2bd9aaa2
uyeong <uyeong21c@gmail.com>
2017-08-24 16:14:43 +0900

    Use media query on SectionCover component

--- .storybook/stories/SectionCover.js
+++ .storybook/stories/SectionCover.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import moment from 'moment';
 import SectionCover from '../../components/SectionCover';
 
 storiesOf('SectionCover', module)
