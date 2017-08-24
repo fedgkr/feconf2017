0125f9ec5e307d1ca028c40aea79e05184430557
uyeong <uyeong21c@gmail.com>
2017-08-24 16:00:01 +0900

    Update Countdown component

--- .storybook/stories/Countdown.js
+++ .storybook/stories/Countdown.js
@@ -6,7 +6,7 @@ import Countdown from '../../components/Countdown';
 storiesOf('Countdown', module)
     .addWithInfo('Default', '', () =>  (
         <div style={{padding: '100px 0', background: '#1c204f'}}>
-            <div style={{maxWidth: '540px', margin: '0 auto'}}>
+            <div style={{maxWidth: '690px', margin: '0 auto'}}>
                 <Countdown eventTime={moment('2017-10-21 10:00:00').unix()}/>
             </div>
         </div>
