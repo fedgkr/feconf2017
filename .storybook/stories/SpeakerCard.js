939514d54ef65e33682821c49e93ccad8b625b4d
uyeong <uyeong21c@gmail.com>
2017-08-27 14:36:59 +0900

    Update stories

--- .storybook/stories/SpeakerCard.js
+++ .storybook/stories/SpeakerCard.js
@@ -4,14 +4,11 @@ import SpeakerCard from '../../components/SpeakerCard';
 
 storiesOf('SpeakerCard', module)
     .addWithInfo('Default', '', () =>  (
-        <div style={{padding: '100px 0', background: '#f0f0f0'}}>
-            <div style={{width: '360px', margin: '0 auto'}}>
-                <SpeakerCard
-                    picture="https://avatars2.githubusercontent.com/u/6267393?v=4&s=400"
-                    name="조은"
-                    company="우아한형제"
-                    desc="우아한형제들에서 프론트엔드 개발을 하고있는 조은입니다. 치킨도 좋아하고 맥주도 좋아하는 데 먹는 걸 다 좋아합니다."
-                />
-            </div>
-        </div>
+        <SpeakerCard
+            style={{width: '360px', margin: '100px auto'}}
+            picture="https://avatars2.githubusercontent.com/u/6267393?v=4&s=400"
+            name="조은"
+            company="우아한형제"
+            desc="우아한형제들에서 프론트엔드 개발을 하고있는 조은입니다. 치킨도 좋아하고 맥주도 좋아하는 데 먹는 걸 다 좋아합니다."
+        />
     ));
