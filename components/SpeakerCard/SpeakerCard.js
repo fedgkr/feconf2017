3807b06a241dacc69984a2d57b80c8005d521621
uyeong <uyeong21c@gmail.com>
2017-08-29 01:52:38 +0900

    Refac style SpeakerCard component

--- components/SpeakerCard/SpeakerCard.js
+++ components/SpeakerCard/SpeakerCard.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 const SpeakerCard = ({className, style, picture, name, company, desc}) => (
     <div className={`speaker-card ${className}`} style={style}>
         <div className="speaker-card__inner">
-            <div className="speaker-card__shadow"/>
             <div className="speaker-card__avatar">
                 <img className="speaker-card__photo" src={picture} alt={`${name}'s picture`}/>
             </div>
