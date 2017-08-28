c4955c6abf7ce43d44c5451e52501533e9d07c6e
uyeong <uyeong21c@gmail.com>
2017-08-29 00:45:31 +0900

    Refac index page

--- components/CardSection/CardSection.js
+++ components/CardSection/CardSection.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 const CardSection = ({className, style, title, subTitle, children}) => (
     <section className={`card-section ${className}`} style={style}>
         <div className="card-section__inner">
-            <div className="card-section__cover">
-                <h2 className="card-section__title">{title}</h2>
-                <p className="card-section__sub-title">{subTitle}</p>
-            </div>
-            <div className="card-section__body">
-                {children}
+            <div className="card-section__card">
+                <div className="card-section__cover">
+                    <h2 className="card-section__title">{title}</h2>
+                    <p className="card-section__sub-title">{subTitle}</p>
+                </div>
+                <div className="card-section__body">
+                    {children}
+                </div>
             </div>
         </div>
     </section>
