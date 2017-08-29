86ef81023bf8bfab4dfee2cb8de14ce3268384e7
uyeong <uyeong21c@gmail.com>
2017-08-29 15:07:33 +0900

    Update Section component

--- components/Section/Section.js
+++ components/Section/Section.js
@@ -2,24 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Section = ({className, style, title, subTitle, children}) => (
-    <div className={`section ${className}`} style={style}>
+    <section className={`section ${className}`} style={style}>
         <div className="section__inner">
-            <div className="section__header">
-                <h2 className="section__title">{title}</h2>
-                <p className="section__sub-title">{subTitle}</p>
-            </div>
+            {title || subTitle ?
+                <div className="section__header">
+                    {title ? <h2 className="section__title">{title}</h2> : null}
+                    {subTitle ? <p className="section__sub-title">{subTitle}</p> : null}
+                </div>
+            : null}
             <div className="section__body">
                 {children}
             </div>
         </div>
-    </div>
+    </section>
 );
 
 Section.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
-    title: PropTypes.string.isRequired,
-    subTitle: PropTypes.string.isRequired
+    title: PropTypes.string,
+    subTitle: PropTypes.string
 };
 
 Section.defaultProps = {
