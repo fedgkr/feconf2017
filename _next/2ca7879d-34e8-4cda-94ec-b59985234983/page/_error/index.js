b374c8d5b046e1a92cf71dfbd71197e95a7afe0c
uyeong <uyeong21c@gmail.com>
2017-08-30 16:09:13 +0900

    Update pages - 2017-08-30 16:09:13

--- _next/2ca7879d-34e8-4cda-94ec-b59985234983/page/_error/index.js
+++ _next/2ca7879d-34e8-4cda-94ec-b59985234983/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([5],[],[756]);
+            var comp = module.exports=webpackJsonp([4],[],[592]);
             return { page: comp.default }
           })
         
--- _next/60c9fe6c-2d97-424c-bbf8-fdd9af93f3a4/page/call-for-speakers/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/call-for-speakers', function() {
-            var comp = module.exports=webpackJsonp([3],[],[753]);
-            return { page: comp.default }
-          })
-        
