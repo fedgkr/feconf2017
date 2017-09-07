13ca2394b575c7421f7be219d6865926438f5afc
uyeong <uyeong21c@gmail.com>
2017-09-07 20:44:36 +0900

    Add google analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,14 @@ class FEConfDocument extends Document {
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
                     <link rel="icon" href="/static/favicon.ico" />
                     <style dangerouslySetInnerHTML={{__html: styles}} />
+                    <script dangerouslySetInnerHTML={{__html: `
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                        ga('create', 'UA-68676515-5', 'auto');
+                        ga('send', 'pageview');
+                    `}}/>
                 </Head>
                 <body>
                     <Main/>
